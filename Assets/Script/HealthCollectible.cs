cb52a58b00d3b748234e874ace99532751fc4ff6
Casey Wells <casey.wells1998@gmail.com>
2019-10-11 08:23:08 -0500

    World building
    I went through the tutorials and tried to build a level.

---
+++ Assets/Script/HealthCollectible.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubiController controller = other.GetComponent<RubiController>();
+
+        if (controller != null)
+
+
+        {
+            if(controller.Health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
