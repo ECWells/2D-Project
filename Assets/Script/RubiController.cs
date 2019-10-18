542cf3dfdc9ec9c00a858a407ad9f44c7f438df2
Casey Wells <casey.wells1998@gmail.com>
2019-10-18 06:13:37 -0500

    Rubi Tutorial
    I struggled with the coding and cannot get the ccog to work

--- Assets/Script/RubiController.cs
+++ Assets/Script/RubiController.cs
@@ -7,24 +7,26 @@ public class RubiController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-
-    public int Health { get { return currentHealth; }}
-    int currentHealth;
-
     public float timeInvincible = 2.0f;
-    bool isInvincible;
-    float invincibleTimer;
+    public GameObject projectilePrefab;
 
+    public int Health {  get { return currentHealth;  }}
+    public int InvicibleTimer { get; private set; }
+    int currentHealth;
+    bool isInvincible;
 
     Rigidbody2D rigidbody2d;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
-
     }
 
     // Update is called once per frame
@@ -33,17 +35,35 @@ public class RubiController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        position = position + move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
         if (isInvincible)
         {
-            invincibleTimer -= Time.deltaTime;
-            if (invincibleTimer < 0)
+            float InvincibleTimer = 0;
+            InvincibleTimer -= Time.deltaTime;
+            if (InvincibleTimer < 0)
                 isInvincible = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
         }
     }
 
@@ -53,13 +73,25 @@ public class RubiController : MonoBehaviour
         {
             if (isInvincible)
                 return;
+
             isInvincible = true;
-            invincibleTimer = timeInvincible;
+            float InvincibleTimer = timeInvincible;
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f,
+    Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
 
