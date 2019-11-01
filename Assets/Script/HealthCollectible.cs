48db598ff3f59f506ed3d74c0cec9963eee0f521
Casey Wells <casey.wells1998@gmail.com>
2019-11-01 13:47:53 -0500

    Build Rubi Project
    I struggled with the coding on this part.

--- Assets/Script/HealthCollectible.cs
+++ Assets/Script/HealthCollectible.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
+    public AudioClip collectedClip;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         RubiController controller = other.GetComponent<RubiController>();
 
         if (controller != null)
-
-
         {
             if(controller.Health < controller.maxHealth)
             {
                 controller.ChangeHealth(1);
                 Destroy(gameObject);
+
+                controller.PlaySound(collectedClip);
             }
         }
     }
